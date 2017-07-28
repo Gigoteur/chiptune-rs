76523042d7d8008d03df6be2510beed015fd033f
Hallucino <d@t0t0.fr>
2017-07-28 14:47:44 +0100

    Add wrapper for chiptune

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,7 +1,12 @@
 #![allow(non_camel_case_types, non_snake_case, dead_code)]
 
-use libc::c_int;
+use libc::{c_void, c_int, c_char};
+
+pub type chiptune_player = *mut c_void;
+pub type chiptune_song = *mut c_void;
 
 extern {
-  pub fn Chiptune_CreatePlayer(sample_rate: c_int) -> c_int;
+  pub fn Chiptune_CreatePlayer(sample_rate: c_int) -> chiptune_player;
+  pub fn Chiptune_LoadSong(player: chiptune_player, path: *const c_char) -> chiptune_song;
+  pub fn Chiptune_PlaySong(player: chiptune_player, son: chiptune_song,  start_position: c_int);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,52 @@
 pub mod ffi;
 
 extern crate libc;
+extern crate sdl2;
+
+use std::ffi::{CString, CStr};
+use libc::{c_void, c_int, c_char};
+use sdl2::mixer;
+
+#[allow(non_snake_case)]
+pub struct Chiptune {
+  P: ffi::chiptune_player,
+}
+
+#[allow(non_snake_case)]
+pub struct ChiptuneSong {
+  S: ffi::chiptune_song,
+}
+
+impl Chiptune {
+  pub fn new() -> Chiptune {
+    let _ = mixer::init(mixer::INIT_MP3 | mixer::INIT_FLAC | mixer::INIT_MOD |
+                                mixer::INIT_FLUIDSYNTH |
+                                mixer::INIT_MODPLUG |
+                                mixer::INIT_OGG)
+                    .unwrap();
+    mixer::open_audio(mixer::DEFAULT_FREQUENCY,
+                      mixer::DEFAULT_FORMAT,
+                      mixer::DEFAULT_CHANNELS,
+                      1024)
+            .unwrap();
+
+
+    unsafe {
+      Chiptune { P: ffi::Chiptune_CreatePlayer(44100) }
+    }
+  }
+
+  pub fn load_song(&mut self, path: String) -> ChiptuneSong {
+    unsafe {
+      let path = CString::new(path).unwrap();
+      ChiptuneSong { S: ffi::Chiptune_LoadSong(self.P, path.as_ptr()) }
+    }
+  }
+
+  pub fn play_song(&mut self, song: ChiptuneSong, start_position: c_int) {
+    unsafe {
+      ffi::Chiptune_PlaySong(self.P, song.S, start_position);
+    }
+  }
+
+}
